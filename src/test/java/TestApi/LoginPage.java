a11d366be3cbc0ffdcee10109456dea5e13f4778
Pavel Davydov <Davydov.works@gmail.com>
2023-11-17 01:34:56 -0500

    add schema + modified code

--- src/test/java/TestApi/LoginPage.java
+++ src/test/java/TestApi/LoginPage.java
@@ -2,15 +2,27 @@ package TestApi;
 
 import Data.DataHelper;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static Data.SQLHelper.cleanAuthCodes;
+import static Data.SQLHelper.cleanDataBase;
+import static com.codeborne.selenide.Selenide.open;
 import static io.restassured.RestAssured.given;
 
 import io.restassured.response.Response;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 
 public class LoginPage {
+
+
     @BeforeAll
     public static void setup() {
 
@@ -24,10 +36,19 @@ public class LoginPage {
 
     }
 
+    @AfterEach
+    void tearDown() {
+        cleanAuthCodes();
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        cleanDataBase();
+    }
+
     @Test
     void shouldGetVerificationPage() {
-        // Given - When - Then
-        // Предусловия
+
         String generatedCode = DataHelper.generateRandomVerificationCode().getCode();
 
         System.out.println("Generated Code: " + generatedCode);
@@ -35,7 +56,7 @@ public class LoginPage {
         Response response = given()
                 .baseUri("http://localhost:9999")
                 .basePath("/api/verification")
-                .body("{\"login\":\"vasya\",\"code\":\""+generatedCode+"\"}")
+                .body("{\"login\":\"vasya\",\"code\":\"" + generatedCode + "\"}")
                 // Выполняемые действия
                 .when()
                 .post()
